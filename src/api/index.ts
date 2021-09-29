f37f059bbf336870ac9cc0989123d88832e8e72a
st63 <st.sam017@gmail.com>
2021-09-29 15:19:39 +0400

    refactor: change baseURL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const request = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'http://api.tvmaze.com',
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
   },
